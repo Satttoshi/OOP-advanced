9874559d596f1231049cb5828431163d52c078b6
Josh <joshuastolle445@gmail.com>
2023-06-06 11:27:59 +0200

    create classes

---
+++ src/main/java/org/module/ComputerScienceStudent.java
@@ -0,0 +1,15 @@
+package org.module;
+
+public class ComputerScienceStudent extends Student {
+
+    private int programmingSkill;
+
+    public void setProgrammingSkill(int programmingSkill) {
+
+        this.programmingSkill = programmingSkill;
+    }
+
+    public int getProgrammingSkill() {
+        return programmingSkill;
+    }
+}
